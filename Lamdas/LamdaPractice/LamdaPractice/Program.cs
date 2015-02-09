cad01ff7f1a352ad46867cc8454abbc7d706e7f2
Juli Anna Burnett <julianna.burnett@gmail.com>
2015-02-09 14:18:31 -0700

    Lambda practice

---
+++ Lamdas/LamdaPractice/LamdaPractice/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaPractice
+{
+    class Program
+    {
+        public static List<string> iceCreamList = new List<string>() { "vanilla", "chocolate", "strawberry", "coffee", "cherry", "praline pecan", "rocky road", "butter pecan", "cookies and cream", "vanilla fudge ripple" };
+        static void Main(string[] args)
+        {
+            string myString = "lambda";
+            //For loop
+            for(int i = 0; i < myString.Length; i++)
+            {
+                Console.WriteLine(myString[i]);
+            }
+
+            //For each example
+            foreach(char letter in myString)
+            {
+                Console.WriteLine(letter);
+            }
+
+            //Lambda expressions
+
+
+            //Where()
+
+
+            //Keeps console open
+            Console.ReadKey();
+        }
+    }
+}
